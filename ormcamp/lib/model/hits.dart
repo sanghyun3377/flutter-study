37df7b0fbabe5bbba713c7e6716054a22d421041
sanghyun3377 <iccompany3377@gmail.com>
2024-01-09 09:49:30 +0900

    pixabay model 생성

---
+++ ormcamp/lib/model/hits.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Hits {
+  String tags;
+  String previewURL;
+  int downloads;
+  int likes;
+  int comments;
+  Hits({
+    required this.tags,
+    required this.previewURL,
+    required this.downloads,
+    required this.likes,
+    required this.comments,
+  });
+}
